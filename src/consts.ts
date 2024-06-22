83d25a2fe9876597f048cd884051be450ef1929e
rimsiw <rimwitkowska@proton.me>
2024-06-22 16:28:25 +0200

    new file:   .gitattributes 	new file:   .gitignore 	new file:   .prettierrc 	new file:   .vscode/extensions.json 	new file:   .vscode/launch.json 	new file:   LICENSE 	new file:   README.md 	new file:   astro.config.mjs 	new file:   package-lock.json 	new file:   package.json 	new file:   pnpm-lock.yaml 	new file:   public/android-chrome-192x192.png 	new file:   public/android-chrome-512x512.png 	new file:   public/apple-touch-icon.png 	new file:   public/favicon-16x16.png 	new file:   public/favicon-32x32.png 	new file:   public/favicon.ico 	new file:   public/fonts/OFL.txt 	new file:   public/fonts/Righteous.eot 	new file:   public/fonts/Righteous.svg 	new file:   public/fonts/Righteous.ttf 	new file:   public/fonts/Righteous.woff 	new file:   public/fonts/Righteous.woff2 	new file:   public/robots.txt 	new file:   public/rss/styles.xsl 	new file:   public/site.webmanifest 	new file:   src/assets/placeholder-hero.png 	new file:   src/assets/placeholder-social.png 	new file:   src/components/BaseHead.astro 	new file:   src/components/Footer.astro 	new file:   src/components/FormattedDate.astro 	new file:   src/components/Header.astro 	new file:   src/components/PostCard.astro 	new file:   src/components/PostsList.astro 	new file:   src/components/ScrollToTopButton.tsx 	new file:   src/components/SeriesCard.astro 	new file:   src/components/SeriesCard.tsx 	new file:   src/components/SeriesList.astro 	new file:   src/components/TableOfContent.astro 	new file:   src/components/TableOfContentHeading.astro 	new file:   src/components/ToggleTheme.astro 	new file:   src/consts.ts 	new file:   src/content/blog/first-post.md 	new file:   src/content/blog/fourth-post.md 	new file:   src/content/blog/markdown-style-guide.md 	new file:   src/content/blog/second-post.md 	new file:   src/content/blog/third-post.md 	new file:   src/content/blog/using-mdx.mdx 	new file:   src/content/config.ts 	new file:   src/content/series/first-series.md 	new file:   src/env.d.ts 	new file:   src/layouts/BaseLayout.astro 	new file:   src/layouts/BlogPostLayout.astro 	new file:   src/pages/blog/[...slug].astro 	new file:   src/pages/blog/index.astro 	new file:   src/pages/index.astro 	new file:   src/pages/rss.xml.js 	new file:   src/pages/series.astro 	new file:   src/pages/tags/[tag].astro 	new file:   src/plugins/remark-reading-time.mjs 	new file:   src/styles/global.css 	new file:   tailwind.config.cjs 	new file:   tsconfig.json

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const SITE_NAME = "RimIs.Gay";
+export const SITE_TITLE = "RimIs.Gay | A personal site by Rim";
+export const SITE_DESCRIPTION =
+  "RimIs.Gay | A personal site by Rim";
+export const TWITTER_HANDLE = "https://twitter.com/abdllah_dev";
+export const GITHUB_HANDLE = "https://github.com/rimsiw";
+export const CLOUDINARY_NAME = "example";
